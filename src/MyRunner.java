ff4d9a8c25e63d7c4f34c641285ab0980bfcdedd
max <maxmolotov@hotmail.it>
2017-06-12 10:35:18 +0200

    Versione finale.

--- src/MyRunner.java
+++ src/MyRunner.java
@@ -2,21 +2,25 @@
  * Created by max on 12/06/17.
  */
 public class MyRunner extends Runner {
-    public MyRunner(String nome, Pista p, int delay) { super(nome, p, delay); }
+    public MyRunner(String nome, Pista p, int delay) {
+        super(nome, p, delay);
+    }
 
     @Override
     public void run() {
-        while(!this.isInterrupted()) {  //A meno che non è arrivato o la corsa è finita
-            try {
-                sleep(delay);
-                p.leave();
-                p = p.getNext();
-                p.step();
-                if(p.arrivato()) {
-                    System.out.println(this.getName()+" è arrivato!");
+        try {
+            while (!this.isInterrupted()) {  //A meno che non è arrivato o la corsa è finita
+                sleep(delay);  //Attesa
+                p.leave();  //Lascia la posizione su cui si trova
+                p = p.getNext();  //Ottiene la posizione successiva
+                p.step();  //Si posizione sulla posizione successiva
+                if (p.arrivato()) {  //Se si è arrivati a destinazione interrompe il thread
+                    System.out.println(this.getName() + " è arrivato!");
                     this.interrupt();
                 }
-            } catch (InterruptedException ignore) { System.out.println(this.getName()+": Ho perso!"); }
+            }
+        } catch (InterruptedException ignore) {  //Se viene preso
+            System.out.println(this.getName() + ": Ho perso!");
         }
     }
 }
