b8e6bdeb9c139f64e6b4f74c04fa17145e5c3547
max <maxmolotov@hotmail.it>
2017-06-12 10:43:50 +0200

    Final

--- src/MyRunner.java
+++ src/MyRunner.java
@@ -9,18 +9,18 @@ public class MyRunner extends Runner {
     @Override
     public void run() {
         try {
-            while (!this.isInterrupted()) {  //A meno che non è arrivato o la corsa è finita
-                sleep(delay);  //Attesa
+            while (!p.arrivato()) {  //A meno che non è arrivato o la corsa è finita
                 p.leave();  //Lascia la posizione su cui si trova
                 p = p.getNext();  //Ottiene la posizione successiva
                 p.step();  //Si posiziona nella casella successiva
-                if (p.arrivato()) {  //Se si è arrivati a destinazione interrompe il thread
-                    System.out.println(this.getName() + " è arrivato!");
-                    this.interrupt();
-                }
+                sleep(delay);  //Attesa
+            }
+            if (p.arrivato()) {  //Se si è arrivati a destinazione interrompe il thread
+                System.out.println(this.getName() + " è arrivato!");
+                this.interrupt();
             }
         } catch (InterruptedException ignore) {  //Se viene preso
-            System.out.println(this.getName() + ": Ho perso!");
+            System.out.println(this.getName().toUpperCase() + " PRESO!");
         }
     }
 }
