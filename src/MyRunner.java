e9587722bc0712448a599524c60418483b63e92b
max <maxmolotov@hotmail.it>
2017-06-12 10:24:03 +0200

    Update.

--- src/MyRunner.java
+++ src/MyRunner.java
@@ -6,16 +6,17 @@ public class MyRunner extends Runner {
 
     @Override
     public void run() {
-        p.step();  //Primissimo passo nella corsa
-        while(p.getNext() != null || this.isInterrupted()) {  //A meno che non è arrivato o la corsa è finita
+        while(!this.isInterrupted()) {  //A meno che non è arrivato o la corsa è finita
             try {
-                sleep(delay);  //Attesa prima di compiere una qualunque azione
-                p.leave();  //Lascio la posizione precedente
-                p = p.getNext();  //Imposto il prossimo passo
-                p.step();  //Metto piede nella posizione successiva
-            } catch (InterruptedException ignore) {System.out.println(this.getName()+": Ho perso!"); }
+                sleep(delay);
+                p.leave();
+                p = p.getNext();
+                p.step();
+                if(p.arrivato()) {
+                    System.out.println(this.getName()+" è arrivato!");
+                    this.interrupt();
+                }
+            } catch (InterruptedException ignore) { System.out.println(this.getName()+": Ho perso!"); }
         }
-        System.out.println(this.getName()+" è arrivato!");
-        this.interrupt();
     }
 }
