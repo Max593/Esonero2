5485464cd2c33cfbf89de6e26df6e260bbe04290
max <maxmolotov@hotmail.it>
2017-06-12 10:12:02 +0200

    Prima prova. Piccole imperfezioni.

--- src/MyRunner.java
+++ src/MyRunner.java
@@ -1,5 +1,21 @@
 /**
  * Created by max on 12/06/17.
  */
-public class MyRunner {
+public class MyRunner extends Runner {
+    public MyRunner(String nome, Pista p, int delay) { super(nome, p, delay); }
+
+    @Override
+    public void run() {
+        p.step();  //Primissimo passo nella corsa
+        while(p.getNext() != null || this.isInterrupted()) {  //A meno che non è arrivato o la corsa è finita
+            try {
+                sleep(delay);  //Attesa prima di compiere una qualunque azione
+                p.leave();  //Lascio la posizione precedente
+                p = p.getNext();  //Imposto il prossimo passo
+                p.step();  //Metto piede nella posizione successiva
+            } catch (InterruptedException ignore) {System.out.println(this.getName()+": Ho perso!"); }
+        }
+        System.out.println(this.getName()+" è arrivato!");
+        this.interrupt();
+    }
 }
